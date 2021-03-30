32198533418374b5d122e67ec9c3ac506a037ece
Ethan <nicebuvr@naver.com>
2021-03-30 10:41:25 +0900

    Update InputTest

--- SampleProject/TransformTest/Assets/Scripts/CarMove.cs
+++ SampleProject/TransformTest/Assets/Scripts/CarMove.cs
@@ -6,6 +6,7 @@ public class CarMove : MonoBehaviour
 {
     float startTime = 0.0f;
     float speed = 3.0f;
+    float rot = 90f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,18 @@ public class CarMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(Vector3.up * -rot * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector3.up * rot * Time.deltaTime);
+
         startTime += Time.deltaTime;
 
         if (startTime <= 3.0f)
