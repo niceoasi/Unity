11ae2f9bdb4b02113953b9f132efe66a6c6f47c5
Ethan <nicebuvr@naver.com>
2021-04-02 10:16:35 +0900

    Update AwakeTest

---
+++ SampleProject/AwakeStartTest/Assets/AwakeStartTest.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeStartTest : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("I am Awake!");
+    }
+
+    void OnEnable()
+    {
+        Debug.Log("I am OnEnable!");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am Start!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
