1cb6e5be424235f4a59114f8f9ef94670f65b130
Ethan <nicebuvr@naver.com>
2021-04-22 01:16:29 +0900

    Update CCTV Test

---
+++ SampleProject/LookAtEx01/Assets/Scripts/CCTVControl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCTVControl : MonoBehaviour
+{
+    Transform pos = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(pos);
+    }
+}
