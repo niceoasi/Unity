13de9dc7d7fdd40a433fb94b8d16599844868c16
Ethan <nicebuvr@naver.com>
2021-08-02 10:34:29 +0900

    Add Dictionary Test

---
+++ SampleProject/DictionaryTest1/Assets/Scripts/DictionaryTest.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+public class DictionaryTest : MonoBehaviour
+{
+
+    Dictionary<string, string> dic = new Dictionary<string, string>()
+    {
+        {"이름", "조상철"}, {"취미", "축구"}
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dic["이름"] = "조단비";
+        dic["이름"] = "조은비";
+
+        StringBuilder str = new StringBuilder();
+
+        foreach (var tmp in dic)
+            str.Append("Key = " + tmp.Key + "Value= " + tmp.Value + "\n");
+
+        Debug.Log(str.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
