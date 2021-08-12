6f4e3e4dbe90740de06b64d75a7f0dc80af92193
Ethan <nicebuvr@naver.com>
2021-08-12 10:07:51 +0900

    Add Player Animation

--- SampleProject/MonsterGame/Assets/Scripts/HeroMove.cs
+++ SampleProject/MonsterGame/Assets/Scripts/HeroMove.cs
@@ -7,15 +7,24 @@ public class HeroMove : MonoBehaviour
     float h, v;
     float speed = 3.0f;
 
+    Animator mAvatar;
+
     public void OnTouchValueChanged(Vector2 stickPos)
     {
         h = stickPos.x;
         v = stickPos.y;
     }
 
+    public void Start()
+    {
+        mAvatar = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        mAvatar.SetFloat("Speed", (h * h + v * v));
+
         if (h != 0f && v != 0f)
         {
             transform.Rotate(0, h, 0);
